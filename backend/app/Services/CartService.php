8eeb49acff8b269705b9711a88a77d82d1fea354
carlosgiil97 <carlosgilterriente@gmail.com>
2024-02-19 08:31:55 +0100

    lógica para crear un carrito nuevo

--- backend/app/Services/CartService.php
+++ backend/app/Services/CartService.php
@@ -38,4 +38,51 @@ class CartService
 
         return $cart->products()->with('categories')->get();
     }
+
+    /**     
+     * Comprueba si ya existe un carrito activo (no completado) para la sesión actual.
+     * Si existe, devuelve el carrito existente. Si no existe, crea un nuevo carrito
+     * y lo devuelve.
+     *
+     * @return \App\Models\Cart Carrito nuevo o el ya relacionado con esa sesión.
+     */
+    public function createCart()
+    {
+        // Obtener el ID de la sesión actual
+        $sessionId = Session::getId();
+
+        $cart = $this->findActiveCart($sessionId);
+
+        if ($cart) {
+            return $cart;
+        }
+
+        return $this->createNewCart($sessionId);
+    }
+
+
+    /**
+     * Busca un carrito activo para la sesión actual.
+     *
+     * @param string $sessionId El ID de la sesión actual.
+     * @return \App\Models\Cart|null El carrito activo o null si no se encuentra.
+     */
+    protected function findActiveCart($sessionId)
+    {
+        return Cart::where('session_id', $sessionId)->where('done', 0)->first();
+    }
+
+    /**
+     * Crea un nuevo carrito para la sesión actual.
+     *
+     * @param string $sessionId El ID de la sesión actual.
+     * @return \App\Models\Cart El nuevo carrito creado.
+     */
+    protected function createNewCart($sessionId)
+    {
+        return Cart::create([
+            'session_id' => $sessionId,
+            'done' => 0,
+        ]);
+    }
 }
